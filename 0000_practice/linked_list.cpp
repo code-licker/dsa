3ba6b4c8d7fee1cad3f75469eef5faa760034f9a
Baba Yaga <baba.yaga@jw.com>
2026-01-04 22:19:39 +0530

    feat: added merge sort

--- 0000_practice/linked_list.cpp
+++ 0000_practice/linked_list.cpp
@@ -1,21 +1,48 @@
-class LL
-{
-    private:
-        int data;
-        LL *next;
 
-    public:
-        LL() {
-            data = 0;
-            next = nullptr;
-        }
-        LL(int new_data) {
-            data = new_data;
-            next = nullptr;
-        }
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        data = 0;
+        next = nullptr;
+    }
+
+    Node(int new_data)
+    {
+        data = new_data;
+        next = nullptr;
+    }
 };
 
-int main() {
+int main()
+{
+    Node *head = new Node(10);
+    Node *n2 = new Node(20);
+    Node *n3 = new Node(30);
+    Node *n4 = new Node(40);
+    Node *n5 = new Node(50);
+    Node *tail = new Node(60);
+
+    head->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+    n5->next = tail;
+
+    Node *temp = head;
+    while (head != nullptr)
+    {
+        cout << temp->data << " --> ";
+        temp = temp->next;
+    }
 
     return 0;
 }
--- 0000_practice/practice.cpp
+++ 0000_practice/practice.cpp
@@ -1,61 +1,38 @@
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int partition(vector<int> &vec, int low, int high)
+vector<int> mergeSortFn(vector<int> &vec, int low, int high, int iteration)
 {
-    int pivot = vec[high];
-    int i = low - 1;
-    for (int j = low; j < high; j++)
+    iteration++;
+    int mid = (low + high) / 2;
+    if (mid >= 0 && iteration <=5)
     {
-        if (vec[j] >= pivot)
+        cout << "iteration --> " << iteration << " ";
+        cout << "mid --> " << mid << " ";
+        cout << "arr --> ";
+        for (int i = low; i <= high; i++)
         {
-            i++;
-            swap(vec[i], vec[j]);
+            cout << vec[i] << ", ";
         }
-    }
-    swap(vec[i + 1], vec[high]);
-    return (i + 1);
-}
+        cout << endl;
 
-vector<int> quickSortFn(vector<int> &vec, int low, int high)
-{
-    if (low < high)
-    {
-        int piv = partition(vec, low, high);
-        quickSortFn(vec, low, piv - 1);
-        quickSortFn(vec, piv + 1, high);
+        mergeSortFn(vec, low, mid, iteration);
+        mergeSortFn(vec, mid + 1, high, iteration);
     }
+
     return vec;
 }
 
-void quickSort(vector<int> &vec)
+void mergeSort(vector<int> &vec)
 {
-    quickSortFn(vec, 0, vec.size()-1);
-}
-
-void insertionSort(vector<int> &vec) {
-    for(int i=1; i<vec.size(); i++) {
-        for (int j=i; j>0; j--) {
-            if(vec[j] > vec[j-1]) {
-                int temp = vec[j];
-                vec[j] = vec[j-1];
-                vec[j-1] = temp;
-            }
-        }
-    }
+    mergeSortFn(vec, 0, vec.size() - 1, 0);
 }
 
 int main()
 {
     vector<int> a = {11, 16, 4, 10, 8, 2, 10, 13, 5, 2, 2, 1, 6, 9, 1, 3, 5};
-    // quickSort(a);
-    insertionSort(a);
-    cout << "------------------" << endl;
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << ", ";
-    cout << endl;
-
+    mergeSort(a);
+    cout << "hi mom";
     return 0;
 }
