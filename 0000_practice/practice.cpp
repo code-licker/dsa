bb43fe2276e135a614ca77bd5d33b25a6d912ff7
Lonkey M. Duffy <44366561+code-licker@users.noreply.github.com>
2026-01-10 11:39:03 +0000

    removed un-necessary files

--- 0000_practice/practice.cpp
+++ 0000_practice/practice.cpp
@@ -2,37 +2,26 @@
 #include <vector>
 using namespace std;
 
-vector<int> mergeSortFn(vector<int> &vec, int low, int high, int iteration)
-{
-    iteration++;
-    int mid = (low + high) / 2;
-    if (mid >= 0 && iteration <=5)
-    {
-        cout << "iteration --> " << iteration << " ";
-        cout << "mid --> " << mid << " ";
-        cout << "arr --> ";
-        for (int i = low; i <= high; i++)
-        {
-            cout << vec[i] << ", ";
-        }
-        cout << endl;
+int main() {
+  int num1 = 10;
+  int num2 = 20;
+  cout << num1 << ", " << num2 << endl;
+  cout << sizeof(num1) << ", " << sizeof(num2) << endl;
+  cout << &num1 << ", " << &num2 << endl;
+  cout << "------------------------------" << endl;
 
-        mergeSortFn(vec, low, mid, iteration);
-        mergeSortFn(vec, mid + 1, high, iteration);
-    }
-
-    return vec;
-}
-
-void mergeSort(vector<int> &vec)
-{
-    mergeSortFn(vec, 0, vec.size() - 1, 0);
-}
-
-int main()
-{
-    vector<int> a = {11, 16, 4, 10, 8, 2, 10, 13, 5, 2, 2, 1, 6, 9, 1, 3, 5};
-    mergeSort(a);
-    cout << "hi mom";
-    return 0;
+  int *num3 = new int(10);
+  int *num4 = num3;
+  cout << num3 << ", " << num4 << endl;
+  cout << *num3 << ", " << *num4 << endl;
+  cout << sizeof(num3) << ", " << sizeof(num4) << endl;
+  cout << &num3 << ", " << &num4 << endl;
+  cout << "--------------Pointer modified----------------" << endl;
+  *num3 = 30;
+  cout << num3 << ", " << num4 << endl;
+  cout << *num3 << ", " << *num4 << endl;
+  cout << sizeof(num3) << ", " << sizeof(num4) << endl;
+  cout << &num3 << ", " << &num4 << endl;
+  cout << "------------------------------" << endl;
+  return 0;
 }
