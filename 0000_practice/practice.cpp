21072e7a0cb6300287fee30603d87509e96d8b39
Lonkey M. Duffy <44366561+code-licker@users.noreply.github.com>
2026-01-12 11:03:57 +0000

    feat: practice merge sort on vectors

--- 0000_practice/practice.cpp
+++ 0000_practice/practice.cpp
@@ -2,34 +2,62 @@
 #include <vector>
 using namespace std;
 
-int pa(vector<int> &v, int l, int h) {
-  int piv = v[h];
-  int i = l - 1;
-  for (int j = l; j < h; j++) {
-    if (v[j] > piv) {
+void me(vector<int> &v, int l, int m, int r) {
+  int s1 = m - l + 1;
+  int s2 = r - m;
+
+  vector<int> L(s1), R(s2);
+
+  for (int i = 0; i < s1; i++)
+    L[i] = v[l + i];
+  for (int j = 0; j < s2; j++)
+    R[j] = v[m + 1 + j];
+
+  int i = 0;
+  int j = 0;
+  int k = l;
+
+  while (i < s1 && j < s2) {
+    if (L[i] >= R[j]) {
+      v[k] = L[i];
       i++;
-      swap(v[i], v[j]);
+    } else {
+      v[k] = R[j];
+      j++;
     }
+    k++;
   }
-  swap(v[i + 1], v[h]);
-  return i + 1;
+
+  while (i < s1) {
+    v[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < s2) {
+    v[k] = R[j];
+    j++;
+    k++;
+  }
 }
 
-vector<int> qsf(vector<int> &v, int l, int h) {
-  if (l < h) {
-    int p = pa(v, l, h);
-    qsf(v, l, p - 1);
-    qsf(v, p + 1, h);
-  }
-  return v;
+void msf(vector<int> &v, int l, int r) {
+  if (l >= r)
+    return;
+  int m = l + ((r - l) / 2);
+  msf(v, l, m);
+  msf(v, m + 1, r);
+  me(v, l, m, r);
 }
 
-void qs(vector<int> &v) { qsf(v, 0, v.size() - 1); }
+void ms(vector<int> &v) {
+  msf(v, 0, v.size() - 1);
+  for (int i = 0; i < v.size(); i++)
+    cout << v[i] << " - ";
+}
 
 int main() {
   vector<int> a = {2, 1, 6, 10, 4, 1, 3, 9, 7};
-  qs(a);
-  for (int i = 0; i < a.size(); i++)
-    cout << a[i] << " - ";
+  ms(a);
   return 0;
 }
